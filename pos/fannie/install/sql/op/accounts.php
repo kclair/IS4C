f591068b75aeb957bfe3f0823f21260256a37c15
k clair <kclair@pickledradish.com>
2011-02-28 11:19:14 -0500

    add CardNo to accounts table for easy lookups

--- pos/fannie/install/sql/op/accounts.php
+++ pos/fannie/install/sql/op/accounts.php
@@ -15,17 +15,19 @@ quite identical across stores.
 
 [Probably] The Same Everywhere:
 * id is an identifying integeger
+* CardNo associates with custdata.CardNo
 * name is the account name, used by Mariposa for decades as the 
 real identifier for accounts
 * balance is the account balance
 * discount is the account discount, an average between all the members' discounts
 * max_balance is the maximum account balance
 
-All of the above info should match MESS at all times.
+All of the above info, except Cardo, should match MESS at all times.
 */
 $CREATE['op.accounts'] = "
 	CREATE TABLE `accounts` (
 	  `id` int(8) NOT NULL,
+          `CardNo` int(8) NOT NULL,
           `name` varchar(255) NOT NULL,
 	  `balance` double NOT NULL default '0',
 	  `discount` smallint(6) default NULL,
