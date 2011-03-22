1325660b6704a1a656948cb11595fd3072616fbc
K Clair <kclair@pickledradish.com>
2011-03-22 18:09:57 -0400

    update import script to: account for ongoing deals; not overwrite existing products. still in testing stage.

--- mariposa-scripts/import_unfi_catalog.rb
+++ mariposa-scripts/import_unfi_catalog.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'mysql'
-#require 'fastercsv'
 require 'yaml'
 
 all_products_file = File.new('./unfi.txt', 'r')
@@ -40,34 +39,45 @@ while (line = all_products_file.gets)
 end
 all_products_file.close
 
+deals_file = File.new('./ogdeals.txt', 'r')
+deal_ogdtypecol = 2
+deal_brandcol = 3
+deal_prodidcol = 5
+deal_typecol = 14
+deal_amountcol = 15
+deals = {}
+while (line = deals_file.gets)
+  row = line.split("\t")
+  ogdtype = row[deal_ogdtypecol]
+  brand = row[deal_brandcol]
+  prod_id = row[deal_prodidcol]
+  identifier = (ogdtype == 1) ? brand : prod_id  # a type of 1 indicates a brand deal
+  deals[identifier] ||= {}
+  deals[identifier]['type'] = (row[deal_typecol] == 1) ? '$' : '%'
+  deals[identifier]['amount'] = row[deal_amountcol]
+end
+deals_file.close
+
 begin
-  dbh = Mysql.real_connect("127.0.0.1", "root", "", "is4c_op")
-  dbh.query("TRUNCATE table products")
-  dbh.query("TRUNCATE table prodExtra")
+  dbh = Mysql.real_connect("localhost", "root", "", "is4c_op")
+#  dbh.query("TRUNCATE table products")
+#  dbh.query("TRUNCATE table prodExtra")
 rescue Mysql::Error => e
   puts "Error code: #{e.errno}"
   puts "Error message: #{e.error}"
   puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
-  die
+  exit
 end
 
 unfi_cats = YAML.load_file('./unfi_categories.yml')
 mfc_products_file = File.new('./mariposa_products.txt','r')
-mfc_cat = 1
-mfc_prodid = 3
-mfc_chkdgt = 4
-
 notfound = 0
 while (line = mfc_products_file.gets)
   row = line.split("\t")
   desc = row[desccol]
   desc.gsub!(/"/, '\"')
-  pack = row[packcol]
-  size = row[sizecol]
-  upc = row[upccol]
-  cost = row[costcol]
   srp = row[srpcol].sub!(/^0+/, '').sub!(/(\d\d)$/, '.\1') || 0
-  cat = row[mfc_cat].to_i
+  cat = row[6].to_i
   if unfi_cats.include?(cat)
     dept = unfi_cats[cat]['dept'] || 0
     subdept = unfi_cats[cat]['subdept'] || 0 
@@ -79,7 +89,7 @@ while (line = mfc_products_file.gets)
     subdept = 0
     fs = 1
   end
-  id = row[mfc_prodid].to_s + row[mfc_chkdgt].to_s
+  id = row[0]
   if id.nil? or !products[id]
     puts 'id or products[id] was nil for '+id
     notfound += 1
@@ -93,13 +103,37 @@ while (line = mfc_products_file.gets)
   pack = products[id]['pack']
   brand = products[id]['brand']
   scale = (dept == 4) ? 1 : 0
+  realcost = cost.to_f / pack.to_f
+  # get special price if there are any deals by brand or product
+  deal = deals[brand] || deals[id]
+  if deal and deal['type'] == '%'
+    special_price = srp * (deal['amount'] / 100)
+  elsif deal and deal['type'] == '$'
+    special_price = srp - deal['amount']
+  else
+    special_price = 'NULL' 
+  end
   begin
-    realcost = cost.to_f / pack.to_f
+    # see if product exists
     dbh ||= Mysql.real_connect("localhost", "is4clane", "is4clane", "opdata")
-    query = "INSERT INTO products (upc, description, normal_price, size, department, subdept, foodstamp, cost, discount, inUse, scale) values (#{upc}, \"#{desc}\", #{srp}, '#{size}', #{dept}, #{subdept}, #{fs}, #{realcost}, 1, 1, #{scale})"
+    query = "SELECT id from products WHERE upc=#{upc}" #dist_id=#{id}"
+    res = dbh.query(query)
+    if res and (row= res.fetch_row)
+      existing_id = row[0]
+    else
+      existing_id = nil
+    end
+    if existing_id
+      # don't overwrite most values if replacing a product
+      query = "REPLACE INTO products (description, normal_price, special_price, size, cost, id) values (\"#{desc}\", #{srp}, #{special_price}, '#{size}', #{realcost}, #{existing_id})"  
+      query2 = "REPLACE INTO prodExtra (upc, distributor, manufacturer, cost, case_cost, case_quantity, dist_id, products_id) values (#{upc}, 'UNFI', \"#{brand}\", #{realcost}, #{cost}, #{pack}, #{id}, #{existing_id})"
+    else 
+      next
+      query = "INSERT INTO products (upc, description, normal_price, special_price, size, department, subdept, foodstamp, cost, discount, inUse, scale) values (#{upc}, \"#{desc}\", #{srp}, #{special_price}, '#{size}', #{dept}, #{subdept}, #{fs}, #{realcost}, 1, 1, #{scale})"
+      query2 = "INSERT INTO prodExtra (upc, distributor, manufacturer, cost, case_cost, case_quantity, dist_id) values (#{upc}, 'UNFI', \"#{brand}\", #{realcost}, #{cost}, #{pack}, #{id})"
+    end
     dbh.query(query)   
-    query = "INSERT INTO prodExtra (upc, distributor, manufacturer, cost, case_cost, case_quantity) values (#{upc}, 'UNFI', \"#{brand}\", #{realcost}, #{cost}, #{pack})"
-    dbh.query(query)   
+    dbh.query(query2)   
   rescue Mysql::Error => e
      puts "Query: #{query}"
      puts "Error code: #{e.errno}"
