e1b2601483d7a3f277002603867a72690522ae91
K Clair <kclair@pickledradish.com>
2011-03-29 17:27:53 -0400

    removed deals from import products script (to be handled in different script); don't overwrite existing products; make products.id the foreign key for prodExtra instead of upc

--- mariposa-scripts/import_unfi_catalog.rb
+++ mariposa-scripts/import_unfi_catalog.rb
@@ -39,27 +39,8 @@ while (line = all_products_file.gets)
 end
 all_products_file.close
 
-deals_file = File.new('./ogdeals.txt', 'r')
-deal_ogdtypecol = 2
-deal_brandcol = 3
-deal_prodidcol = 5
-deal_typecol = 14
-deal_amountcol = 15
-deals = {}
-while (line = deals_file.gets)
-  row = line.split("\t")
-  ogdtype = row[deal_ogdtypecol]
-  brand = row[deal_brandcol]
-  prod_id = row[deal_prodidcol]
-  identifier = (ogdtype == 1) ? brand : prod_id  # a type of 1 indicates a brand deal
-  deals[identifier] ||= {}
-  deals[identifier]['type'] = (row[deal_typecol] == 1) ? '$' : '%'
-  deals[identifier]['amount'] = row[deal_amountcol]
-end
-deals_file.close
-
 begin
-  dbh = Mysql.real_connect("localhost", "root", "", "is4c_op")
+  dbh = Mysql.real_connect("localhost", "root", "", "is4c_op_test")
 #  dbh.query("TRUNCATE table products")
 #  dbh.query("TRUNCATE table prodExtra")
 rescue Mysql::Error => e
@@ -82,6 +63,7 @@ while (line = mfc_products_file.gets)
     dept = unfi_cats[cat]['dept'] || 0
     subdept = unfi_cats[cat]['subdept'] || 0 
     fs = unfi_cats[cat]['nofs'] ? 0 : 1
+    tax = unfi_cats[cat]['tax'] ? 1 : 0
   else
     puts 'no category for '+cat.to_s+' for row '+row.inspect
     exit
@@ -104,19 +86,10 @@ while (line = mfc_products_file.gets)
   brand = products[id]['brand']
   scale = (dept == 4) ? 1 : 0
   realcost = cost.to_f / pack.to_f
-  # get special price if there are any deals by brand or product
-  deal = deals[brand] || deals[id]
-  if deal and deal['type'] == '%'
-    special_price = srp * (deal['amount'] / 100)
-  elsif deal and deal['type'] == '$'
-    special_price = srp - deal['amount']
-  else
-    special_price = 'NULL' 
-  end
   begin
     # see if product exists
-    dbh ||= Mysql.real_connect("localhost", "is4clane", "is4clane", "opdata")
-    query = "SELECT id from products WHERE upc=#{upc}" #dist_id=#{id}"
+    dbh ||= Mysql.real_connect("localhost", "is4clane", "is4clane", "is4c_op_test")
+    query = "SELECT products_id from prodExtra WHERE dist_id=#{id}" #dist_id=#{id}"
     res = dbh.query(query)
     if res and (row= res.fetch_row)
       existing_id = row[0]
@@ -125,17 +98,23 @@ while (line = mfc_products_file.gets)
     end
     if existing_id
       # don't overwrite most values if replacing a product
-      query = "REPLACE INTO products (description, normal_price, special_price, size, cost, id) values (\"#{desc}\", #{srp}, #{special_price}, '#{size}', #{realcost}, #{existing_id})"  
-      query2 = "REPLACE INTO prodExtra (upc, distributor, manufacturer, cost, case_cost, case_quantity, dist_id, products_id) values (#{upc}, 'UNFI', \"#{brand}\", #{realcost}, #{cost}, #{pack}, #{id}, #{existing_id})"
+      puts 'updating product '+upc.to_s
+      query = "UPDATE products SET description=\"#{desc}\", normal_price=#{srp}, size='#{size}', cost=#{realcost} WHERE id=#{existing_id}"
+      query2 = "UPDATE prodExtra SET distributor='UNFI', manufacturer=\"#{brand}\", cost=#{realcost}, case_cost=#{cost}, case_quantity=#{pack}, dist_id=#{id} WHERE products_id=#{existing_id}"
+      dbh.query(query)
+      dbh.query(query2)
     else 
-      next
-      query = "INSERT INTO products (upc, description, normal_price, special_price, size, department, subdept, foodstamp, cost, discount, inUse, scale) values (#{upc}, \"#{desc}\", #{srp}, #{special_price}, '#{size}', #{dept}, #{subdept}, #{fs}, #{realcost}, 1, 1, #{scale})"
-      query2 = "INSERT INTO prodExtra (upc, distributor, manufacturer, cost, case_cost, case_quantity, dist_id) values (#{upc}, 'UNFI', \"#{brand}\", #{realcost}, #{cost}, #{pack}, #{id})"
+      puts 'adding product '+upc.to_s
+      query = "INSERT INTO products (upc, description, normal_price, size, department, subdept, foodstamp, cost, discount, inUse, scale, tax) values (#{upc}, \"#{desc}\", #{srp}, '#{size}', #{dept}, #{subdept}, #{fs}, #{realcost}, 1, 1, #{scale}, #{tax})"
+      dbh.query(query)   
+      res = dbh.query("SELECT id from products where upc=#{upc}")
+      row = res.fetch_row
+      existing_id = row[0] 
+      query2 = "INSERT INTO prodExtra (upc, distributor, manufacturer, cost, case_cost, case_quantity, dist_id, products_id) values (#{upc}, 'UNFI', \"#{brand}\", #{realcost}, #{cost}, #{pack}, #{id}, #{existing_id})"
     end
-    dbh.query(query)   
     dbh.query(query2)   
   rescue Mysql::Error => e
-     puts "Query: #{query}"
+     puts "Query: #{query}\n#{query2}"
      puts "Error code: #{e.errno}"
      puts "Error message: #{e.error}"
      puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
